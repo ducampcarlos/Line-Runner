b0e54e7650880af77db1d8e4ff87e8fb509a211d
ducampcarlos <ducampcarlos@gmail.com>
2025-04-01 20:17:01 -0300

    Added: Player Script and basic functionality.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    float playerYPosition;
+
+    private void Start()
+    {
+        playerYPosition = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+#if UNITY_ANDROID || UNITY_EDITOR
+        // Detects if the screen was touched, and inverts player position.
+        if (Touchscreen.current.primaryTouch.press.wasPressedThisFrame)
+        {
+            playerYPosition = -playerYPosition;
+            var newPos = new Vector3(transform.position.x, playerYPosition, transform.position.z);
+            transform.position = newPos;
+        }
+#elif UNITY_STANDALONE || UNITY_EDITOR
+        // Detects if the space key was pressed, and inverts player position.
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            playerYPosition = -playerYPosition;
+            var newPos = new Vector3(transform.position.x, playerYPosition, transform.position.z);
+            transform.position = newPos;
+        }
+#endif
+    }
+}
